a8155c05c91048d353101d867854f16797625c8d
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-05-13 17:15:19 -0500

    before hw#10

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
-    
+    public float movementSpeed = 40.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rb = this.GetComponent<Rigidbody>();
+        print(MasterData.count);
+        MasterData md = new MasterData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MasterData.count++;
+            SceneManager.LoadScene("DungeonRoom");
+        }
     }
 }
