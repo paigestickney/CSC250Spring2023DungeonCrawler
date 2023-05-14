dca7ff2a3584a314f3c6be8f9f991f36fbae23e0
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-05-14 00:40:28 -0500

    hw #10

--- Dungeon Crawler/Assets/Scripts/DungeonSetup.cs
+++ Dungeon Crawler/Assets/Scripts/DungeonSetup.cs
@@ -10,7 +10,7 @@ public class DungeonSetup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.northExit.SetActive(false);
+        this.northExit.SetActive(true);
     }
 
     // Update is called once per frame
