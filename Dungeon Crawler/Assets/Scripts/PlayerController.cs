d6da3b41749e3e18f2af97ecd6ff9fb32faea9ae
paigestickney <75600165+paigestickney@users.noreply.github.com>
2023-05-14 01:32:18 -0500

    before hw#11

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,34 +8,63 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
+    private bool isMoving;
 
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        print(MasterData.count);
         MasterData md = new MasterData();
+        this.isMoving = false;
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Exit"))
+        {
+            if (other.gameObject == this.northExit)
+            {
+                MasterData.whereDidIComeFrom = "north";
+            }
+            else if (other.gameObject == this.southExit)
+            {
+                MasterData.whereDidIComeFrom = "south";
+            }
+            else if (other.gameObject == this.eastExit)
+            {
+                MasterData.whereDidIComeFrom = "east";
+            }
+            else if (other.gameObject == this.westExit)
+            {
+                MasterData.whereDidIComeFrom = "west";
+            }
+            SceneManager.LoadScene("DungeonRoom");
+        }
+    }   
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && this.isMoving == false)
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if (Input.GetKeyDown(Keycode.DownArrow) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.DownArrow) && this.isMoving == false)
         {
             this.rb.AddForce(this.southExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && this.isMoving == false)
         {
             this.rb.AddForce(this.westExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && this.isMoving == false)
         {
             this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
     }
 }
